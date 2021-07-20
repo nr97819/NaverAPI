bdaa657e96aa886d48183dfe85bf4c9a7f7e5a48
2018111320 <happy0652@naver.com>
2021-07-20 14:48:20 +0900

    김민지 refector

--- DataRefine.py
+++ DataRefine.py
@@ -1,13 +1,37 @@
 import json
 
+# Summary:
+# 통합 검색어 트렌드 데이터 정제, 김민지
+# param:
+# resultData - 정제할 데이터
+# returns:
+# refinedData - 정제된 데이터
 def DataRefining(resultData):
-    refinedData = json.loads(resultData)
+    refinedData = []
+    refinedLen = 0
+
+    refinedResult = json.loads(resultData)
+    refinedLen = len(refinedResult['results'][0]['data'])
+
+    refinedData.append({'len':refinedLen,
+                        'data':[]
+    })
+
+    for data in refinedResult['results'][0]['data']:
+        refinedPeriod = data["period"]
+        refinedRatio = data["ratio"]
+
+        refinedData[0]['data'].append({'period' : refinedPeriod,
+                            'ratio' : refinedRatio
+        })
+
+
     # 경로는 본인 PC에 맞게 설정
     writePath = r'C:/NaverAPI/NaverAPI/search_naver_data.json'
     with open(writePath, 'w', encoding='utf-8') as filedata:
         rJson = json.dumps( refinedData, 
                             indent=4,
-                            sort_keys=False, # 오히려 섞임
+                            sort_keys=False,
                             ensure_ascii=False )
         filedata.write(rJson)
     return refinedData
