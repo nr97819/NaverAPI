bdd5c08fbd7294f117f73cd4ddf247caba267b82
WonhaWoo <xoox8831@gmail.com>
2021-07-20 13:55:07 +0900

    Main수정

--- Main.py
+++ Main.py
@@ -1,32 +1,14 @@
-import DataAuth
 import DataCrawl
 import DataRefine
 import DataVisual
-import urllib.request
 
-def Main():
-
-    url = "https://openapi.naver.com/v1/datalab/search" # Crawl Part
-
-    # 출력할 연도 입력 받기
-    startDate, endDate, timeUnit = DataCrawl.GetPeriod() # Static Value ...
+def Gradient(ratio):
     
-    # 출력할 keyWord 입력 받기 <-----------------
-    body = DataCrawl.GetKeyword(startDate, endDate, timeUnit)  # Static Value ...
+    pass
 
-    request = urllib.request.Request(url)
-    DataAuth.Authentication(request)
-    response = urllib.request.urlopen(request, data=body.encode("utf-8"))
-    resCode = response.getcode()
-
-    if(resCode==200):
-        response_body = response.read()
-        resultData = (response_body.decode('utf-8'))
-    else:
-        print("Error Code:" + resCode)
-    
-    refinedData = DataRefine.DataRefining(DataCrwal.resultData)
-
+def Main():    
+    # 출력할 keyWord 입력 받기 <-----------------   
+    refinedData = DataRefine.DataRefining(DataCrawl.GetCrawlingresult)
     DataVisual.PrintInfo(refinedData)
 
 if __name__ == '__main__':
