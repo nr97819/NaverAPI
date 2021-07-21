137e36403284dd35d7238617187b2d6fa1412f6a
soonang <nr97819@naver.com>
2021-07-21 10:27:40 +0900

    main 입력 받기 위한 crawl 리스트 생성 및 준비

--- DataCrawl.py
+++ DataCrawl.py
@@ -1,10 +1,23 @@
+from typing import Text
 from DataAuth import Authentication
 from datetime import date, datetime # 현재 시각 반환 라이브러리
 import urllib.request
 
 date = str(datetime.now())
 date = date[:date.rfind(' ')]
-resultData = 'default'
+
+url = "https://openapi.naver.com/v1/datalab/search"
+
+keyWord = []
+keyWord1 = '[\"클라우드\", \"cloud\"]'
+keyWord2 = '[\"클라우드 보안\", \"cloud security\"]'
+
+def SetKeyWord(text):
+    split1, split2 = text.split(',')
+    temp = '[\"%s\", \"%s\"]' % (split1, split2)
+    global keyWord1
+    keyWord1 = temp
+    # return text
 
 def GetPeriod():
     startDate = "2016-01-01" # 네이버 API 검색 가능일
@@ -13,33 +26,30 @@ def GetPeriod():
 
     return startDate, endDate, timeUnit
 
-def GetKeyword(startDate, endDate, timeUnit):
-
-    keyWord1 = '["클라우드"]'
-    keyWord2 = '["클라우드 보안"]'
-
+def GetFormat(startDate, endDate, timeUnit, keyWord):
     body = '''{\"startDate\":\"%s\",\"endDate\":\"%s\",\"timeUnit\":\"%s\",\"keywordGroups\":[
-                {\"groupName\":\"그룹 1\",\"keywords\":%s}, 
-                {\"groupName\":\"그룹 2\",\"keywords\":%s}
-                ]}''' % (startDate, endDate, timeUnit, keyWord1, keyWord2)
-
+                {\"groupName\":\"그룹1\",\"keywords\":%s}]}''' % (startDate, endDate, timeUnit, keyWord)
     return body
 
-url = "https://openapi.naver.com/v1/datalab/search"
+def GetHttpResponse(keyWord):
+    startDate, endDate, timeUnit = GetPeriod()
+    body = GetFormat(startDate, endDate, timeUnit, keyWord)
 
-startDate, endDate, timeUnit = GetPeriod()
-body = GetKeyword(startDate, endDate, timeUnit)
+    request = urllib.request.Request(url)
+    request = Authentication(request)
+    response = urllib.request.urlopen(request, data=body.encode("utf-8"))
+    resCode = response.getcode()
 
-request = urllib.request.Request(url)
-request = Authentication(request)
-response = urllib.request.urlopen(request, data=body.encode("utf-8"))
-resCode = response.getcode()
+    if(resCode==200):
+        response_body = response.read()
+        return response_body.decode('utf-8')
+    else:
+        print("Error Code:" + resCode) # 예외 처리
+        return None
 
-if(resCode==200):
-    response_body = response.read()
-    resultData = (response_body.decode('utf-8'))
-else:
-    print("Error Code:" + resCode)
-
+'GetCrawlingResult()의 #만 지우면 목표결과 완성!'
 def GetCrawlingResult():
-    return resultData
+    return GetHttpResponse(keyWord1)#, GetHttpResponse(keyWord2) # 각각의 JSON 파일
+
+# inputValue = input('첫번째 키워드 2개를 입력해주세요!\n예시) 오징어,땅콩\n')
+# keyWord1 = SetKeyWord(inputValue)
