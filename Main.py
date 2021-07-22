5e80681f39eb1bf1b46c10c51248819f5986257a
WonhaWoo <xoox8831@gmail.com>
2021-07-22 09:16:21 +0900

    add test code

--- Main.py
+++ Main.py
@@ -8,10 +8,13 @@ def Gradient(ratio):
 def Main():    
     # 출력할 keyWord 입력 받기 <-----------------  
     refinedData = list()
+    test = [['클라우드', 'cloud'], ['클라우드 보안', 'cloud security']]
     for i in range(2):
-        DataCrawl.SetKeyWord(input('검색어 입력\n').split(','))
+        # DataCrawl.SetKeyWord(input('검색어 입력\n').split(','))
+        DataCrawl.SetKeyWord(test[i])
         refinedData.append(DataRefine.DataRefining(DataCrawl.GetCrawlingResult()))
-    DataVisual.PrintInfo(refinedData)
+    # DataVisual.PrintInfo(refinedData)
+    DataVisual.ChangeFind(refinedData)
 
 if __name__ == '__main__':
     Main()
