636ef31c4f2613b8a2ea89f253774467e2ed4f58
soonang <nr97819@naver.com>
2021-07-22 10:00:38 +0900

    변경내용 수정 2 (2개 입력 재활용)

--- Main.py
+++ Main.py
@@ -10,21 +10,25 @@ def Main():
     # 출력할 keyWord 입력 받기 <-----------------  
     resultData = list()
     inputValue = ''
+    listForRecycle = []
 
     for i in range(2):
-        inputValue = input('2개의 키워드를 입력하세요. (ex. 클라우드,cloud)\n')
+        inputValue = input(i, '번째 그룹의 키워드를 2개 입력하세요. (ex. 클라우드, cloud)\n')
         DataCrawl.SetKeyWord(inputValue.split(','))
+        listForRecycle.append(inputValue) # 재활용을 위해 List 사용
 
         crawledData = DataCrawl.GetCrawlingResult()          # 1. 크롤링
         refinedData = DataRefine.DataRefining(crawledData)   # 2. 정제
         resultData.append(refinedData)
 
-    DataVisual.PrintInfo(resultData)                            # 3. 시각화
+    DataVisual.PrintInfo(resultData)                         # 3. 시각화
 
     # 07.21. 신규 추가 ------------------------------
-    DataCrawlByDate.query = inputValue # input 재활용
-    directlyCrawledData = DataCrawlByDate.GetNewsCrawlingData() # <------------------- 워드 클라우드용 데이터
-    DataCrawlByDate.GetExcelResultQuery(directlyCrawledData)
+    for i in range(2):
+        print('[', listForRecycle[i], '에 대한 상세검색 ]')
+        DataCrawlByDate.query = listForRecycle[i] # input 재활용
+        directlyCrawledData = DataCrawlByDate.GetNewsCrawlingData() # <------------------- 워드 클라우드용 데이터
+        DataCrawlByDate.GetExcelResultQuery(directlyCrawledData)
 
 if __name__ == '__main__':
     Main()
