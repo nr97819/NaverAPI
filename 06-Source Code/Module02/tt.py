8be862f3cd12169fd57f65250a0572e28706b582
soonang <nr97819@naver.com>
2021-08-17 16:48:32 +0900

    html 코드 및 tt (메인)파일 공유

--- 06-Source Code/Module02/tt.py
+++ 06-Source Code/Module02/tt.py
@@ -5,23 +5,22 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
-@app.route('/')
-def hello_world():
-    return render_template('home.html')
+# @app.route('/')
+# def hello_world():
+#     return render_template('home.html')
 
-@app.route('/test')
-def test():
-    return render_template('get.html')
+# @app.route('/test')
+# def test():
+#     return render_template('get.html')
 
-# @app.route('/get', methods=['get'])
-@app.route('/post', methods=['post'])
+@app.route('/', methods=['post'])
 def get():
     keyword1 = request.form['keyword1']
     keyword2 = request.form['keyword2']
     enkeyword1 = translator.translate(keyword1, dest='en')
     enkeyword2 = translator.translate(keyword2, dest='en')
     result = str([keyword1, enkeyword1.text, keyword2, enkeyword2.text])
-    return result
+    return render_template('home.html', result=result)
 
 if __name__ == '__main__':
     app.run()
