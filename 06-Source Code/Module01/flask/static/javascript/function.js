c6a8708dad06d2a9f6da33eede00e05ccdfb568e
WonhaWoo <xoox8831@gmail.com>
2021-08-19 17:12:56 +0900

    ㅋㅋ된다 돼 와우

--- 06-Source Code/Module01/flask/static/javascript/function.js
+++ 06-Source Code/Module01/flask/static/javascript/function.js
@@ -4,13 +4,13 @@ function input_Text(){
 }
 
 function zoomIn(event) {
+    event.target.style.zIndex = 1;
     event.target.style.transform = "scale(2.5)";
-    event.target.style.zIndex = 2;
     event.target.style.transition = "all 0.3s";
 }
 
 function zoomOut(event) {
-    event.target.style.transform = "scale(1)";
     event.target.style.zIndex = 0;
+    event.target.style.transform = "scale(1)";
     event.target.style.transition = "all 0.2s";
 }
