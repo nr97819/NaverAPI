cb6cc2c21f583e405b6f62a7da6c6f383ca2ec25
soonang <nr97819@naver.com>
2021-08-19 13:24:08 +0900

    상우 HTML 통합 테스트

--- 06-Source Code/Module01/flask/run.py
+++ 06-Source Code/Module01/flask/run.py
@@ -10,7 +10,7 @@ app.config['JSON_AS_ASCII'] = False
 def hello_world():
     return render_template('home.html', welcome='welcome.png')
 
-@app.route('/', methods=['post'])
+@app.route('/result', methods=['post'])
 def get():
     keyWord1 = request.form['keyword1']
     keyWord2 = request.form['keyword2']
@@ -19,7 +19,7 @@ def get():
     resultKeyword = str([keyWord1, enkeyWord1.text, keyWord2, enkeyWord2.text])
     resultKeyword = [keyWord1, enkeyWord1.text, keyWord2, enkeyWord2.text]
     Main.Main(resultKeyword)
-    return render_template('home.html', trend='trend.png', graph='graph.png')
+    return render_template('result.html', trend='trend.png', graph='graph.png')
 
 if __name__ == '__main__':
     app.run(debug=True)
